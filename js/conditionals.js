f8e2d8733b350474efd98b34f389679a3654c8e4
Bapreet <75432662+Bapreet@users.noreply.github.com>
2024-01-31 10:54:41 -0500

    changed to windy icon

--- js/conditionals.js
+++ js/conditionals.js
@@ -31,7 +31,9 @@ function setWeather() {
 		//change weather icon
 		icon.setAttribute("src", "images/sunny.svg");
 	}else if (choice === "rainy"){
-		icon.setAttribute("src", "images/rainy.svg");
+		icon.setAttribute("src", "images/rainy.svg");	
+	}else if (choice === "windy"){
+		icon.setAttribute("src", "images/windy.svg");	
 	}
 	/* STEP 3: Add a nested IF/ELSE statement inside the 'sunny' condition above that tests for temperature, and if it is equal to or greater than 15, turn the page background orange, otherwise turn it lightblue */
 
